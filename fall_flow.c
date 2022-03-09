69fa6f0c6b05f48e294792d32fad524948187ef0
greedymelon <74475072+greedymelon@users.noreply.github.com>
2022-03-09 15:54:02 +0100

    Create fall_flow.c
    first commit: not tested

---
+++ fall_flow.c
@@ -0,0 +1,29 @@
+#include "../includes/cluster.h"
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+t_axial fall_flow (int q, int r, int color)
+{
+    t_axial hex_pos;
+	hex_pos.q = q;
+	hex_pos.r = r;
+	int hash_index = hash(hext_pos);
+	while (hash_array[hash_index] == NULL)
+	{
+		insert(hex_pos, color);
+		if (hex_pos.q - hex_pos.r == r)
+		{//check the last position available on the map
+			hash_array[hash_index] = 1;
+			return ;
+		}
+		hex_pos.r++;
+		hash_index = hash(hext_pos);
+		if (hash_array[hash_index] == NULL)
+			delite(hex_pos);
+	}
+	hex_pos.r--;
+	hash_arrai[hash(hext_pos)] = 1;
+}
